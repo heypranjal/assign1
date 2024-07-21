b0a3d379ec0391d7380e7cdec53ebd98215da6af
Pranjal Tripathi <talk2pranjaltripathi@gmail.com>
2024-07-21 17:08:52 +0530

    checkemail

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-function findAge() {
-   const dateOfBirth = document.getElementById("dob").value;
-   const dob = new Date(dateOfBirth);
-   const curnt = new Date();
-   const userAge = curnt.getFullYear()-dob.getFullYear()
-   console.log(userAge);
-   if (userAge<18) {
-    alert("age is Invalid")
-   } 
-};
+function calculateAge(){
+   const enteredDob = document.getElementById('dob').value;
+   const dob = new Date(enteredDob);
+   const today = new Date();
+
+   let age = today.getFullYear() - dob.getFullYear()
+
+   if(age>=18){
+       document.getElementById('result').innerHTML = "You are eligible to register";
+       return true;
+   }
+   else{
+       alert("You are not eligible to register");
+       document.getElementById('result').innerHTML = "You are not eligible to register";
+       return false;
+}
+}
+function checkemail() {
+   const enteremail=document.getElementById('Email').value
+   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+   const isValid= emailPattern.test(enteremail)
+   if(!isValid){
+ alert("You are not eligible")
+   }
+  
+   }
+   function selectstates(){
+    
+   }
